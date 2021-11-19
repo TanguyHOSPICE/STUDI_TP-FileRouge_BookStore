71ff1750c13ff6fbf1e4d1fefb25218a145a884a
hospice tanguy <htcvhs971@gmail.com>
2021-11-19 11:35:35 -0400

    Create Book with objects + array

--- Assets/JS/javascript.js
+++ Assets/JS/javascript.js
@@ -1,4 +1,5 @@
 //Créer un livre
+
 //Creation sous forme de variables en attendant de voir les tblx
 let book;
 let title = "Les Misérables";
@@ -7,3 +8,37 @@ let price = 14.95;
 let category = "Drame";
 let releaseDate = new Date(1997,05,23);
 let nbSales =345;
+
+//Créer un livre avec des objets et tblx
+let books = [
+    {
+     title: "Les Misérables",
+     author: "Victor Hugo",
+     price: 14.95,
+     category: "Drame",
+     releaseDate: new Date(1997,05,23),
+     nbSales:345,
+     resume:"Jean Valjean a été condamné au bagne en 1795 pour le vol d'un pain, jugement qui symbolise l'oppression qu'impose une société injuste à une population écrasée ; L'Inspecteur Javert, policier, incarne l'intransigeance de la société bourgeoise. Pas de rémission pour un ancien forçat, pas de grâce pour Valjean.",
+     votes:[5,4,5],
+    },
+    {
+     title: "Les Misérables",
+     author: "Victor Hugo",
+     price: 14.95,
+     category: "Drame",
+     releaseDate: new Date(1997,05,23),
+     nbSales:345,
+     resume:"",
+     votes:[8,5,4,3,5],
+    },
+    {
+     title: "Les Misérables",
+     author: "Victor Hugo",
+     price: 14.95,
+     category: "Drame",
+     releaseDate: new Date(1997,05,23),
+     nbSales:345,
+     resume:"",
+     votes:[5,6,7,4,5],
+    },
+];
