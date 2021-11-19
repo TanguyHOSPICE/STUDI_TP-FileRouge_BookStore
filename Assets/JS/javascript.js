9522d350a4c2cde7e3cd53714dfec4df651d0f0f
hospice tanguy <htcvhs971@gmail.com>
2021-11-19 12:36:34 -0400

    Create Book with objects + array in a json directory

--- Assets/JS/javascript.js
+++ Assets/JS/javascript.js
@@ -10,35 +10,10 @@ let releaseDate = new Date(1997,05,23);
 let nbSales =345;
 
 //Créer un livre avec des objets et tblx
-let books = [
-    {
-     title: "Les Misérables",
-     author: "Victor Hugo",
-     price: 14.95,
-     category: "Drame",
-     releaseDate: new Date(1997,05,23),
-     nbSales:345,
-     resume:"Jean Valjean a été condamné au bagne en 1795 pour le vol d'un pain, jugement qui symbolise l'oppression qu'impose une société injuste à une population écrasée ; L'Inspecteur Javert, policier, incarne l'intransigeance de la société bourgeoise. Pas de rémission pour un ancien forçat, pas de grâce pour Valjean.",
-     votes:[5,4,5],
-    },
-    {
-     title: "Les Misérables",
-     author: "Victor Hugo",
-     price: 14.95,
-     category: "Drame",
-     releaseDate: new Date(1997,05,23),
-     nbSales:345,
-     resume:"",
-     votes:[8,5,4,3,5],
-    },
-    {
-     title: "Les Misérables",
-     author: "Victor Hugo",
-     price: 14.95,
-     category: "Drame",
-     releaseDate: new Date(1997,05,23),
-     nbSales:345,
-     resume:"",
-     votes:[5,6,7,4,5],
-    },
+let books = []; // TODO:Importer le json
+
+//Création Tbl d'objet pour panier avec 1 id et la quantité
+let cart =[
+    {id:"0", qtt:1},
+    {id:"1", qtt:3},
 ];
