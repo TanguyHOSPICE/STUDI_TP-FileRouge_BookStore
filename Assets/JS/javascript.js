c420a057043fa3a11b77af28d28b182d018f47d9
hospice tanguy <htcvhs971@gmail.com>
2021-11-19 10:59:58 -0400

    Create Book with variables

---
+++ Assets/JS/javascript.js
@@ -0,0 +1,9 @@
+//Créer un livre
+//Creation sous forme de variables en attendant de voir les tblx
+let book;
+let title = "Les Misérables";
+let author = "Victor Hugo";
+let price = 14.95;
+let category = "Drame";
+let releaseDate = new Date(1997,05,23);
+let nbSales =345;
